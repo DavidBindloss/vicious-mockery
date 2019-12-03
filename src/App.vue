729cef23f8529373d208c28f94fa2e4fb877c73a
David Bindloss <davidb@strategicdata.com.au>
2019-12-03 13:43:39 +1100

    Adding social share / id permalinks

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
   <div class="container">
-    <div class="space spacer-top"></div>
     <div id="app" class="space">
       <p class="insult">{{ insult }}</p>
-      <button class="button is-info" @click.prevent="generate">Insult me again!</button>
-    </div>
-    <div class="space spacer-bottom">
-      
+      <button class="button is-info is-rounded" @click.prevent="generate">
+        <span class="icon">
+          <font-awesome-icon :icon="['fas', 'redo']" />
+        </span>
+        <span>Again</span>
+      </button>
+      <button class="button is-primary is-rounded" @click.prevent="send">
+        <span class="icon">
+          <font-awesome-icon :icon="['fas', 'share-alt']" />
+        </span>
+        <span>Share</span>
+      </button>
+      <p>{{ ids }}</p>
     </div>
     <div class="icons">
       <a href="https://github.com/DavidBindloss/vicious-mockery">
         <font-awesome-icon :icon="['fab', 'github']" size="3x" />
       </a>
+      <a href>
+        <font-awesome-icon :icon="['fas', 'share-alt']" size="3x" />
+      </a>
+       <social-sharing url="https://vuejs.org/"
+                      title="The Progressive JavaScript Framework"
+                      description="Intuitive, Fast and Composable MVVM for building interactive interfaces."
+                      quote="Vue is a progressive framework for building user interfaces."
+                      hashtags="vuejs,javascript,framework"
+                      twitter-user="vuejs"
+                      inline-template>
+                      <div>
+                           <network network="facebook">
+        <i class="fa fa-facebook"></i> Facebook
+      </network>
+       <network network="reddit">
+        <i class="fa fa-reddit"></i> Reddit
+      </network>
+      </div>
+       </social-sharing>
     </div>
   </div>
-  
 </template>
 
 <script>
 import data from "./assets/data.json";
-
-let getPart = section => {
-  let arr = data[`part_${section}`];
-  return arr[Math.floor(Math.random() * arr.length)];
-};
 
 export default {
   name: "app",
@@ -31,25 +52,59 @@ export default {
     this.generate();
   },
   methods: {
+    getRIndex() {
+      return Math.floor(Math.random() * 49);
+    },
+
+    getIdx(f, p = 0) {
+      return parseInt(
+        f
+          .split("")
+          .splice(p, 2)
+          .join("")
+      );
+    },
+
     generate() {
-      this.insult = `Thou ${getPart("one")} ${getPart("two")} ${getPart(
-        "three"
-      )}!`;
+      // Is there a querystring?
+      let query = this.$route.query.id;
+      // let one, two, three;
+      let indices;
+      if (query && query.length === 6) {
+        indices = Array.from(Array(3)).map(
+          (x, i) => this.getIdx(query, i * 2) + 50 * i
+        );
+      } else {
+        indices = Array.from(Array(3)).map((x, i) => this.getRIndex() + 50 * i);
+      }
+
+      let [one, two, three] = indices.map(x => data[x]);
+      this.insult = `Thou ${one} ${two} ${three}!`;
+      this.ids = indices.map((x, i) => x - 50 * i);
+
+      if (this.$route.query.id) this.$router.replace("/");
     }
   },
   data() {
     return {
-      insult: ""
+      insult: "",
+      ids: []
     };
   }
 };
 </script>
 
 <style lang="scss">
+
 body,
 html {
   height: 100%;
 }
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -59,28 +114,23 @@ html {
 }
 
 .container {
+
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100%;  
-  margin: 0 !important;
-  width: 100% !important;
-  max-width: 100% !important;
+  flex-direction: column;
+  height: 100%;
+
   .space {
-    max-width: 50%;
+    button:first-of-type {
+      margin-right: 15px;
+    }
+
     .insult {
       font-weight: bold;
-      font-size: 1.4rem;
+      font-size: 1.6rem;
       margin-bottom: 15px;
     }
-  }
-
-  .spacer-top {
-    align-self: flex-start;
-  }
-
-  .spacer-bottom {
-    align-self: flex-end;
   }
 
   .icons {
@@ -90,6 +140,7 @@ html {
     a {
       color: #5e5e5e;
       transition: color 0.3s ease;
+      margin-right: 15px;
       &:hover {
         color: #363636;
       }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "bulma";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import VueRouter from 'vue-router'
+import "./assets/style.scss"
+
+import SocialSharing from "vue-social-sharing";
+ 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faShareAlt, faRedo } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faGithub);
+library.add(faShareAlt);
+library.add(faRedo);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+Vue.use(SocialSharing);
+
+let routes = [
+   { path: '/', component: App, name: 'home'}
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
